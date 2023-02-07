6c3c1f7098ce141b21650ec2fb3168ebf2ae2590
cosenzam <michaelcosenza8@gmail.com>
2023-02-07 17:49:23 -0500

    css stylesheet, login requirements, user profile and settings routes, navbar different when logged in or out

--- forms.py
+++ forms.py
@@ -1,13 +1,21 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField
-from wtforms.validators import InputRequired, Length, EqualTo
+from wtforms.validators import InputRequired, Length, EqualTo, Optional
 
 class CreateAccountForm(FlaskForm):
-    username = StringField('Username', validators = [InputRequired(), Length(min = 6, max = 32)])
+    user_name = StringField('Username', validators = [InputRequired(), Length(min = 6, max = 32)])
     email = StringField('Email', validators = [InputRequired(), Length(min = 8, max = 64)])
     password = PasswordField('Password', [InputRequired(), EqualTo('confirm', message='Passwords must match')])
     confirm  = PasswordField('Repeat Password')
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators = [InputRequired(), Length(min = 6, max = 32)])
+    user_name = StringField('Username', validators = [InputRequired(), Length(min = 6, max = 32)])
     password = PasswordField('Password', validators = [InputRequired()])
+
+class UserProfileForm(FlaskForm):
+    user_bio = StringField('Enter user bio:', validators = [Optional(), Length(min = 0, max = 128)])
+
+class UserSettingsForm(FlaskForm):
+    email = StringField('Change email', validators = [Optional(), Length(min = 8, max = 64)])
+    password = PasswordField('New Password', [Optional(), EqualTo('confirm', message='Passwords must match')])
+    confirm  = PasswordField('Repeat Password')
