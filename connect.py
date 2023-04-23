6452922578802db1ade835d2f38c66ae8849db97
Carlos Preising <carlos@carlospreising.com>
2023-04-23 07:46:20 -0400

    carlos-cloud: updated

--- connect.py
+++ connect.py
@@ -20,14 +20,18 @@ def db_connect():
     MYSQL_PASS = credentials["MYSQL_PASS"]
     MYSQL_DB = credentials["MYSQL_DB"]
 
-    GCP_USER = credentials["GCP_USER"]
-    GCP_PASS = credentials["GCP_PASS"]
-    GCP_DB = credentials["GCP_DB"]
-    GCP_CONNECTION_NAME = credentials["GCP_CONNECTION_NAME"]
 
     if os.environ.get('GAE_INSTANCE'):
+
+        credentials = json.load(os.environ.get("CLOUD_SQL_CREDENTIALS_SECRET"))
+
+        
+        GCP_USER = credentials["GCP_USER"]
+        GCP_PASS = credentials["GCP_PASS"]
+        GCP_DB = credentials["GCP_DB"]
+        GCP_CONNECTION_NAME = credentials["GCP_CONNECTION_NAME"]
         engine = create_engine(
-            f"""mysql+pymysql://{GCP_USER}:{GCP_PASS}@{GCP_DB}?unix_socket=/cloudsql/{GCP_CONNECTION_NAME}""",
+            f"""mysql+pymysql://{GCP_USER}:{GCP_PASS}@/{GCP_DB}?unix_socket=/cloudsql/{GCP_CONNECTION_NAME}""",
             echo = False
         )
 
