742238c6acd8de4acc55a4454346081d140e1de3
Carlos Preising <carlos@carlospreising.com>
2023-04-23 07:53:32 -0400

    carlos-cloud: updated

--- connect.py
+++ connect.py
@@ -5,34 +5,29 @@ from sqlalchemy import create_engine
 
 def db_connect():
 
+    path = "credentials.json"
+
+    #I'm storing it here, but keeping original functionality
+
     
-    if not os.environ.get('GAE_INSTANCE'):
-
-        path = "credentials.json"
-
-        #I'm storing it here, but keeping original functionality
+    try:
         if not pathlib.Path(path).is_file():
             path = "secrets/credentials.json"
-        
-        try:
-            with open(path) as file:
-                credentials = json.load(file)
+        with open(path) as file:
+            credentials = json.load(file)
 
-            MYSQL_IP = credentials["MYSQL_IP"]
-            MYSQL_PORT = credentials["MYSQL_PORT"]
-            MYSQL_USER = credentials["MYSQL_USER"]
-            MYSQL_PASS = credentials["MYSQL_PASS"]
-            MYSQL_DB = credentials["MYSQL_DB"]
+        MYSQL_IP = credentials["MYSQL_IP"]
+        MYSQL_PORT = credentials["MYSQL_PORT"]
+        MYSQL_USER = credentials["MYSQL_USER"]
+        MYSQL_PASS = credentials["MYSQL_PASS"]
+        MYSQL_DB = credentials["MYSQL_DB"]
 
-            engine = create_engine(
-                f"""mysql+pymysql://{MYSQL_USER}:{MYSQL_PASS}@{MYSQL_IP}:{MYSQL_PORT}/{MYSQL_DB}""",
-                echo = False
-            )
-        except:
-            None
+        engine = create_engine(
+            f"""mysql+pymysql://{MYSQL_USER}:{MYSQL_PASS}@{MYSQL_IP}:{MYSQL_PORT}/{MYSQL_DB}""",
+            echo = False
+        )
 
-
-    else:
+    except:
         credentials = json.load(os.environ.get("CLOUD_SQL_CREDENTIALS_SECRET"))
 
         GCP_USER = credentials["GCP_USER"]
@@ -44,7 +39,5 @@ def db_connect():
             f"""mysql+pymysql://{GCP_USER}:{GCP_PASS}@/{GCP_DB}?unix_socket=/cloudsql/{GCP_CONNECTION_NAME}""",
             echo = False
         )
-
-
 
     return engine
