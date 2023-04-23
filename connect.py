83bfba3095f7416856757dd699003f28fce67dab
Carlos Preising <carlos@carlospreising.com>
2023-04-23 06:37:10 -0400

    carlos-cloud: updated

--- connect.py
+++ connect.py
@@ -1,5 +1,6 @@
 import json
 import pathlib
+import os
 from sqlalchemy import create_engine
 
 def db_connect():
@@ -18,17 +19,23 @@ def db_connect():
     MYSQL_USER = credentials["MYSQL_USER"]
     MYSQL_PASS = credentials["MYSQL_PASS"]
     MYSQL_DB = credentials["MYSQL_DB"]
-    # if os.environ.get ('GAE_INSTANCE'):
-    # MYSQL_IP = credentials["MYSQL_IP"]
-    # MYSQL_PORT = credentials["MYSQL_PORT"]
-    # MYSQL_USER = credentials["MYSQL_USER"]
-    # MYSQL_PASS = credentials["MYSQL_PASS"]
-    # MYSQL_DB = credentials["MYSQL_DB"]
 
-    engine = create_engine(
-        f"""mysql+pymysql://{MYSQL_USER}:{MYSQL_PASS}@{MYSQL_IP}:{MYSQL_PORT}/{MYSQL_DB}""",
-        echo = False
-    )
+    GCP_USER = credentials["GCP_USER"]
+    GCP_PASS = credentials["GCP_PASS"]
+    GCP_DB = credentials["GCP_DB"]
+    GCP_CONNECTION_NAME = credentials["GCP_CONNECTION_NAME"]
+
+    if os.environ.get('GAE_INSTANCE'):
+        engine = create_engine(
+            f"""mysql+pymysql://{GCP_USER}:{GCP_PASS}@/{GCP_DB}?unix_socket=/cloudsql/{GCP_CONNECTION_NAME}""",
+            echo = False
+        )
+
+    else:
+        engine = create_engine(
+            f"""mysql+pymysql://{MYSQL_USER}:{MYSQL_PASS}@{MYSQL_IP}:{MYSQL_PORT}/{MYSQL_DB}""",
+            echo = False
+        )
 
 
     return engine
