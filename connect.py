f77010008ffc5919e86b066d6031fb4da62493da
Carlos Preising <carlos@carlospreising.com>
2023-04-23 07:50:28 -0400

    carlos-cloud: updated

--- connect.py
+++ connect.py
@@ -5,41 +5,46 @@ from sqlalchemy import create_engine
 
 def db_connect():
 
-    path = "credentials.json"
+    
+    if not os.environ.get('GAE_INSTANCE'):
 
-    #I'm storing it here, but keeping original functionality
-    if not pathlib.Path(path).is_file():
-        path = "secrets/credentials.json"
+        path = "credentials.json"
 
-    with open(path) as file:
-        credentials = json.load(file)
+        #I'm storing it here, but keeping original functionality
+        if not pathlib.Path(path).is_file():
+            path = "secrets/credentials.json"
+        
+        try:
+            with open(path) as file:
+                credentials = json.load(file)
 
-    MYSQL_IP = credentials["MYSQL_IP"]
-    MYSQL_PORT = credentials["MYSQL_PORT"]
-    MYSQL_USER = credentials["MYSQL_USER"]
-    MYSQL_PASS = credentials["MYSQL_PASS"]
-    MYSQL_DB = credentials["MYSQL_DB"]
+            MYSQL_IP = credentials["MYSQL_IP"]
+            MYSQL_PORT = credentials["MYSQL_PORT"]
+            MYSQL_USER = credentials["MYSQL_USER"]
+            MYSQL_PASS = credentials["MYSQL_PASS"]
+            MYSQL_DB = credentials["MYSQL_DB"]
+
+            engine = create_engine(
+                f"""mysql+pymysql://{MYSQL_USER}:{MYSQL_PASS}@{MYSQL_IP}:{MYSQL_PORT}/{MYSQL_DB}""",
+                echo = False
+            )
+        except:
+            None
 
 
-    if os.environ.get('GAE_INSTANCE'):
-
+    else:
         credentials = json.load(os.environ.get("CLOUD_SQL_CREDENTIALS_SECRET"))
 
-        
         GCP_USER = credentials["GCP_USER"]
         GCP_PASS = credentials["GCP_PASS"]
         GCP_DB = credentials["GCP_DB"]
         GCP_CONNECTION_NAME = credentials["GCP_CONNECTION_NAME"]
+
         engine = create_engine(
             f"""mysql+pymysql://{GCP_USER}:{GCP_PASS}@/{GCP_DB}?unix_socket=/cloudsql/{GCP_CONNECTION_NAME}""",
             echo = False
         )
 
-    else:
-        engine = create_engine(
-            f"""mysql+pymysql://{MYSQL_USER}:{MYSQL_PASS}@{MYSQL_IP}:{MYSQL_PORT}/{MYSQL_DB}""",
-            echo = False
-        )
 
 
     return engine
