91b96a8df59283815bbcb98d6604c2887183e465
Carlos Preising <carlos@carlospreising.com>
2023-04-25 07:37:21 -0400

    carlos-cloud2: basic func.

--- connect.py
+++ connect.py
@@ -23,18 +23,19 @@ def db_connect():
     INSTANCE_CONNECTION_NAME = f"{PROJECT_ID}:{REGION}:{INSTANCE_NAME}"
 
     connector = Connector()
-
-    conn = connector.connect(
-       INSTANCE_CONNECTION_NAME,
-       "pymysql",
-       user=USER,
-       password=PASSWORD,
-       db=DB_NAME
-    )
+    def get_conn():
+        conn = connector.connect(
+        INSTANCE_CONNECTION_NAME,
+        "pymysql",
+        user=USER,
+        password=PASSWORD,
+        db=DB_NAME
+        )
+        return conn
 
     engine = create_engine(
         f"mysql+pymysql://",
-        creator=conn
+        creator=get_conn
     )
 
     return engine
