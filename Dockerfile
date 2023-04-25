575e48a158f7d01e6bafa1c3eee1ad6845f09e9a
Carlos Preising <carlos@carlospreising.com>
2023-04-25 04:09:42 -0400

    carlos-cloud2: setting up local docker build

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM python:3.11-slim
+# Copy local code to the container image
+COPY . /app
+
+# Sets the working directory
+WORKDIR /app
+
+# Upgrade PIP
+RUN pip install --upgrade pip
+
+#Install python libraries from requirements.txt
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Set $PORT environment variable
+ENV PORT 8080
+
+# Run the web service on container startup
+CMD exec gunicorn --bind :$PORT --workers 1 --threads 8 --timeout 0 run:app
--- connect.py
+++ connect.py
@@ -4,24 +4,21 @@ from sqlalchemy import create_engine
 
 def db_connect():
 
-    path = "credentials.json"
-
-    #I'm storing it here, but keeping original functionality
-    if not pathlib.Path(path).is_file():
-        path = "secrets/credentials.json"
+    path = "secrets/credentials_gcp.json"
 
     with open(path) as file:
         credentials = json.load(file)
 
-    MYSQL_IP = credentials["MYSQL_IP"]
-    MYSQL_PORT = credentials["MYSQL_PORT"]
-    MYSQL_USER = credentials["MYSQL_USER"]
-    MYSQL_PASS = credentials["MYSQL_PASS"]
-    MYSQL_DB = credentials["MYSQL_DB"]
+    USER = credentials["USER"]
+    PASSWORD = credentials["PASSWORD"]
+    PUBLIC_IP = credentials["PUBLIC_IP"]
+    PROJECT_ID = credentials["PROJECT_ID"]
+    DB_NAME = credentials["DB_NAME"]
+    INSTANCE_NAME = credentials["INSTANCE_NAME"]
 
     engine = create_engine(
-        f"""mysql://{MYSQL_USER}:{MYSQL_PASS}@{MYSQL_IP}:{MYSQL_PORT}/{MYSQL_DB}""",
-        echo = False
+        f"""
+            mysql+pymysql://{USER}:{PASSWORD}@{PUBLIC_IP}/{DB_NAME}?unix_socket=/cloudsql/{PROJECT_ID}:{INSTANCE_NAME}"""
     )
 
     return engine
